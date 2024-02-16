d49fca825398199f9b85b04d40cf1ba9d8a46f89
Mysterium422 <mysterium422@gmail.com>
2024-02-16 03:56:54 -0500

    Splits data into train and test sets

--- wikipedia_lib.py
+++ wikipedia_lib.py
@@ -1,24 +1,25 @@
 import wikipedia
 import re
 import random
-import sys
-
-with open("companies.txt", "r") as file:
-  companies = [line.strip() for line in file.readlines()]
 
 def getWikiPage(company):
   company_title = wikipedia.search(company + " company")[0]
   return wikipedia.page(title=company_title, auto_suggest=False)
 
+def readFileLines(filepath):
+  with open(filepath, "r") as file:
+    return [line.strip() for line in file.readlines()]
+
 def main():
-  for idx, company in enumerate(companies[200:], start=200):
+  companies = readFileLines("companies.txt")
+  for idx, company in enumerate(companies[:50], start=0):
     print(idx)
     wiki = getWikiPage(company)
     text = wiki.content
     split_lines = (text.split("\n"))
 
     event_section_keywords = ["history", "creation", "leadership", "corporate", "acquisitions", "growth", "finance", "financial", "lawsuits", "litigation", "legal"]
-    date_pattern = r'^(On|In|By|As of)\s+(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{4}'
+    date_pattern = r'^(On|In|By|As of)\s+(January|February|March|April|May|June|July|August|September|October|November|December)\s+(\d{4})'
     positive_sentences = []
     negative_sentences = []
 
@@ -43,7 +44,7 @@ def main():
               sentence = sentence[match.end():]
               while sentence.startswith(" ") or sentence.startswith(","):
                 sentence = sentence[1:]
-              positive_sentences.append(sentence)
+              positive_sentences.append((sentence, int(match.group(3))))
         else:
             sentences = line.split(".")
             for sentence in sentences:
@@ -61,7 +62,7 @@ def main():
       return string.lower().startswith(company.lower()) or string.lower().startswith(wiki.title.lower()) or string.lower().startswith("the company")
 
     if len(negative_sentences) > len(positive_sentences):
-      pos_company_count = sum(1 for string in positive_sentences if startswithcompany(string))
+      pos_company_count = sum(1 for string, year in positive_sentences if startswithcompany(string))
       neg_company_count = sum(1 for string in negative_sentences if startswithcompany(string))
       while len(negative_sentences) > len(positive_sentences):
         if neg_company_count > pos_company_count:
@@ -84,11 +85,11 @@ def main():
               negative_sentences[:-amtToRemove]
               break
     elif len(positive_sentences) > len(negative_sentences):
-      pos_company_count = sum(1 for string in positive_sentences if startswithcompany(string))
+      pos_company_count = sum(1 for string, year in positive_sentences if startswithcompany(string))
       neg_company_count = sum(1 for string in negative_sentences if startswithcompany(string))
       while len(positive_sentences) > len(negative_sentences):
         if pos_company_count > neg_company_count:
-          for i, sentence in enumerate(positive_sentences):
+          for i, (sentence, year) in enumerate(positive_sentences):
             if startswithcompany(sentence):
               del positive_sentences[i]
               pos_company_count -= 1
@@ -98,7 +99,7 @@ def main():
               positive_sentences[:-amtToRemove]
               break
         else:
-          for i, sentence in enumerate(positive_sentences):
+          for i, (sentence, year) in enumerate(positive_sentences):
             if not startswithcompany(sentence):
               del positive_sentences[i]
               break
@@ -106,12 +107,12 @@ def main():
               amtToRemove = len(positive_sentences) - len(negative_sentences)
               positive_sentences[:-amtToRemove]
               break
-    with open("positive.txt", "a") as file:
-      for sentence in positive_sentences:
-        file.write(sentence + "\n")
-    with open("negative.txt", "a") as file:
-      for sentence in negative_sentences:
-        file.write(sentence + "\n")
+
+    pos_test = [sentence for sentence, year in positive_sentences if year >= 2023]
+    pos_train = [sentence for sentence, year in positive_sentences if year < 2023]
+    random.shuffle(negative_sentences)
+    neg_test = negative_sentences[:len(pos_test)]
+    neg_train = negative_sentences[len(pos_test):]
 
 if __name__ == "__main__":
     main()
